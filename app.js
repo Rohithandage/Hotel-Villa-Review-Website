97d1d4b183ef4dc256a162f9c572ad3bc297c9d7
Rohit Handage <106050288+Rohithandage@users.noreply.github.com>
2024-03-29 01:18:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const listingsRouter = require("./routes/listing.js")
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
- const MONGO_URL = "mongodb://127.0.0.1:27017/wonder";
+ const MONGO_URL = "*************";
 
 
 
